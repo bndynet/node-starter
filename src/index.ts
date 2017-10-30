a73083a76d40432e47159efd18fa5c6302a3fb91
Bing Zhang <bingzha@cisco.com>
2017-10-30 18:21:31 +0800

    ejs support and logger

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
+import * as expressLayouts from 'express-ejs-layouts';
+
+import { Logger } from './lib/logger';
 
 const app = express();
+const logger = new Logger('./log');
 
 app.use(bodyParser.json());
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+app.set('layout extractScripts', true);
+app.set('view engine', 'ejs');
+app.set('layout', 'layouts/main');
 
-app.get('/', (req: any, res: any) => {
-    res.send({name: 'Bendy'});
+app.get('/', (req: Request, res: any) => {
+    res.render('pages/index', {user: 'Bendy', layout: 'layouts/layout'});
+});
+app.get('/home', (req: any, res: any) => {
+    logger.write('request home');
+    res.render('pages/home', {user: 'Bendy'});
+});
+app.get('/about', (req: any, res: any) => {
+    logger.write('request about');
+    res.render('pages/about', {user: 'Bendy'});
 });
 
 const server = app.listen(3000, () => {
@@ -17,5 +34,7 @@ const server = app.listen(3000, () => {
         host = '127.0.0.1';
     }
 
-    console.log('Server is listening at http://%s:%s', host, port);
+    const msg = `Server is listening at http://${host}:${port}`;
+    logger.write(msg);
+    console.log(msg);
 });
